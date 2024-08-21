39370aea2db1ac0566b4c988a2cbbe2e0be598ff
Hamza Maach <hamzamaach56@gmail.com>
2024-08-21 15:11:46 +0100

    create 'models' package that contain global structs

---
+++ models/artist.go
@@ -0,0 +1,15 @@
+package models
+
+// Artist structure for holding artist data
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+}
+
+type ArtistsPageData struct {
+	Artists []Artist
+}
---
+++ models/error.go
@@ -0,0 +1,8 @@
+package models
+
+// Data struct to hold data for error
+type ErrorData struct {
+	Error   string
+	Code    int
+	Message string
+}
