92a7e9f0514faf074c52fa726132194441e12cad
Hamza Maach <hamzamaach56@gmail.com>
2024-08-22 16:09:45 +0100

    get the port for the server

--- main.go
+++ main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"groupie_tracker/config"
 	"groupie_tracker/handlers"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default to port 8080 if not set
+	}
+
 	fs := http.FileServer(http.Dir("./assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.HandleFunc("/", handlers.MainHandler)
 	http.HandleFunc("/artist/{id}", handlers.ArtistHandler)
-	fmt.Println("Starting the server on : http://localhost"+config.Port)
-	err := http.ListenAndServe(config.Port, nil)
-	if err != nil {
-		fmt.Println("500 | Internal Server Error :", err)
-	}
+	fmt.Println("Starting the server on : http://localhost" + config.Port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// err := http.ListenAndServe(config.Port, nil)
+	// if err != nil {
+	// 	fmt.Println("500 | Internal Server Error :", err)
+	// }
 }
