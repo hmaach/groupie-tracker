99e695f3ee6612fc614b7b29b6c0e4f52466c4a7
Youssefhajjaoui <youssefhajjaoui67@gmail.com>
2024-08-31 18:27:38 +0100

    check search

--- handlers/handle_Search.go
+++ handlers/handle_Search.go
@@ -21,28 +21,26 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		RenderError(w, http.StatusMethodNotAllowed, "405 | Method Not Allowed: Use GET")
 		return
 	}
+	id := []int{}
 	Key := r.FormValue("Search")
 	var Newdata models.CombinedData
 	for _, l := range data.Artists {
 		if (strings.Contains(Key, l.Name)) || (strings.Contains(l.FirstAlbum, Key)) || (strings.Contains(Key, strconv.Itoa(l.CreationDate))) {
-			if !Double(l.ID, Newdata) {
-				fmt.Println(l.ID)
-				Newdata.Artists = append(Newdata.Artists, l)
+			if !exist(id, l.ID) {
+				id = append(id, l.ID)
 			}
 		}
 		for _, M := range l.Members {
-			if strings.Contains(M, Key) && !Double(l.ID, Newdata) {
-				fmt.Println(l.ID)
-				Newdata.Artists = append(Newdata.Artists, l)
+			if strings.Contains(M, Key) && !exist(id, l.ID) {
+				id = append(id, l.ID)
 			}
 		}
 	}
 
-	id := []int{}
 	for _, j := range data.Locations.Index {
 		for _, J := range j.Locations {
 			if strings.Contains(J, Key) {
-				if !exist(id, j.ID, Newdata) {
+				if !exist(id, j.ID) {
 					id = append(id, j.ID)
 				}
 			}
@@ -50,13 +48,13 @@ func Search(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println(id)
 	for _, ids := range id {
-		if !Double(ids, Newdata) {
-			new, err := utils.FetchArtist(strconv.Itoa(ids))
-			if err != nil {
-				RenderError(w, http.StatusInternalServerError, "500 | Failed to render artist detail page.")
-			}
-			Newdata.Artists = append(Newdata.Artists, new)
+
+		new, err := utils.FetchArtist(strconv.Itoa(ids))
+		if err != nil {
+			RenderError(w, http.StatusInternalServerError, "500 | Failed to render artist detail page.")
 		}
+		Newdata.Artists = append(Newdata.Artists, new)
+
 	}
 	if err := RenderTemplate(w, "index.html", http.StatusOK, Newdata); err != nil {
 		RenderError(w, http.StatusInternalServerError, "500 | Failed to render the page.")
@@ -64,9 +62,9 @@ func Search(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func exist(ids []int, nb int, data models.CombinedData) bool {
+func exist(ids []int, nb int) bool {
 	for _, id := range ids {
-		if id == nb || Double(id, data) {
+		if id == nb {
 			return true
 		}
 	}
