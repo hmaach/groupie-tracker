b688ad39d1bffc247316c5b4e2f99f5e64c87fb5
Hamza Maach <hamzamaach56@gmail.com>
2024-09-08 17:53:23 +0100

    add filter card to the index

--- handlers/handle_filter.go
+++ handlers/handle_filter.go
@@ -1,8 +1,8 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"groupie_tracker/data"
 )
@@ -33,13 +33,27 @@ func FilterHandler(w http.ResponseWriter, r *http.Request) {
 	// filteredData := utils.FilterData(data, creationDateMin, creationDateMax, firstAlbumMin, firstAlbumMax, membersMin, membersMax)
 
 	// Marshal the filtered data into JSON
-	jsonData, err := json.Marshal(data.CombinedData)
-	if err != nil {
-		http.Error(w, "500 | Internal Server Error", http.StatusInternalServerError)
-		return
-	}
+	// jsonData, err := json.Marshal(data.CombinedData)
+	// if err != nil {
+	// 	http.Error(w, "500 | Internal Server Error", http.StatusInternalServerError)
+	// 	return
+	// }
 
 	// Set response header and write JSON data
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonData)
+	// w.Header().Set("Content-Type", "application/json")
+	// w.Write(jsonData)
+
+	// Set the Type field based on the number of members
+	for i := range data.Artists {
+		if len(data.Artists[i].Members) == 1 {
+			data.Artists[i].Type = "Artist"
+		} else {
+			data.Artists[i].Type = "Group of " + strconv.Itoa(len(data.Artists[i].Members))
+		}
+	}
+
+	if err := RenderTemplate(w, "index.html", http.StatusOK, data.Artists); err != nil {
+		RenderError(w, http.StatusInternalServerError, "500 | Failed to render the page.")
+		return
+	}
 }
